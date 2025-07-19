91fa47e78f2f43a7d39eb5fb2e309bc480e777b1
echo <play3step@naver.com>
2025-07-19 16:50:15 +0900

    chore : tailwind css 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
     </div>
   );
 }
